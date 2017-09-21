41c42f1fa4df84d8bde8bf0f9ee3cf411f4ab39e
soarflat <soarflat777@gmail.com>
2017-09-21 14:45:47 +0900

    [update] app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { createStore } from './store'
+import { createRouter } from './router'
+import { sync } from 'vuex-router-sync'
+import titleMixin from './util/title'
+import * as filters from './util/filters'
+
